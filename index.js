d9801d712201d606747ec6a151e6f96999f7023f
WindTR <43916344+WindTR@users.noreply.github.com>
2020-10-22 12:04:13 +0300

    First Upload

---
+++ index.js
@@ -0,0 +1,59 @@
+//Made by WindTR
+//https://github.com/WindTR/
+//https://discord.gg/kBcxMga/
+//https://wind.js.org/
+
+
+const electron = require('electron');
+const path = require("path");
+const url = require("url")
+const { Menu, protocol } = require("electron");
+
+const { app, BrowserWindow, ipcMain, ipcRenderer } = electron;
+
+let mainwindow;
+
+app.on("ready", () => {
+    
+    mainwindow = new BrowserWindow({
+        webPreferences:{
+            nodeIntegration: true
+        },
+        width: 740,
+        height: 540
+    });
+
+    
+
+    console.log(process.platform + process.arch +" ")
+
+    mainwindow.loadURL(
+        url.format({
+            pathname: path.join(__dirname, "views/index.html"),
+            protocol: "file",
+            slashes: true
+        })
+    );
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+    Menu.setApplicationMenu(mainMenu)
+    
+});
+    const mainMenuTemplate = [
+        {
+            label: "Dosya",
+            submenu: [
+                {
+                    label: "Geliştirici Araçları",
+                    click: async () => {
+                        mainwindow.webContents.openDevTools()
+                    }
+                },
+                {
+                    label: "Çıkış",
+                    click: async () => {
+                        process.exit(0)
+                    }
+                }
+            ]
+        }
+    ]
